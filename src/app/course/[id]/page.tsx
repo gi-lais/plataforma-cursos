b74b04b9af0eeb931ebc166f8b94b1add645ca27
Giovanna <gi.laisoliveiraa@icloud.com>
2025-05-16 18:04:21 -0300

    🐛 bugfix: Corrigindo bug de Id

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -1,24 +1,33 @@
 'use client'
 
+import { use } from 'react'
 import { CoursePlayer } from '@/components/CoursePlayer/CoursePlayer'
 import { CoursePreview } from '@/components/CoursePreview/CoursePreview'
 import { useUser } from '@/context/UserContext'
 import courses from '@/data/courses.json'
 import { notFound } from 'next/navigation'
 
-
-interface Props {
-  params: { id: string }
+interface Params {
+  id: string
 }
 
-export default function CoursePage({ params }: Props) {
+export default function CoursePage({ params }: { params: Promise<Params> }) {
   const { hasCourse } = useUser()
-  const courseId = Number(params.id)
+  const { id } = use(params)
+  const courseId = Number(id)
   const course = courses.find(c => c.id === courseId)
 
   if (!course) return notFound()
 
   return hasCourse(courseId)
-    ? <CoursePlayer title={course.title} description={course.description}/>
-    : <CoursePreview id={course.id} title={course.title} description={course.description} price={course.price} created_at={course.created_at}/>
+    ? <CoursePlayer title={course.title} description={course.description} />
+    : (
+      <CoursePreview
+        id={course.id}
+        title={course.title}
+        description={course.description}
+        price={course.price}
+        created_at={course.created_at}
+      />
+    )
 }
